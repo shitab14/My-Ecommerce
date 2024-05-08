6fc9776b0ed50ddca99bb70f065cd569e3758417
Shitab Mir <shitabmir@gmail.com>
2024-05-08 10:30:20 +0600

    [DataBinding Added]

--- app/src/main/java/com/mir/myecommerce/MainActivity.kt
+++ app/src/main/java/com/mir/myecommerce/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mir.myecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.mir.myecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.iBtnLanguageSwitch.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
