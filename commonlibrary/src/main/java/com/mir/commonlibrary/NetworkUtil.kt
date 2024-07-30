59bd4a6fc5cb3085d0e3c73c945b965366c36eed
shitabmir <shitabmir@gmail.com>
2024-07-30 18:48:16 +0600

    [Is VPN Active Check]

--- commonlibrary/src/main/java/com/mir/commonlibrary/NetworkUtil.kt
+++ commonlibrary/src/main/java/com/mir/commonlibrary/NetworkUtil.kt
@@ -2,6 +2,8 @@ package com.mir.commonlibrary
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 
 /**
 Created by Shitab Mir on 8/5/24.
@@ -22,4 +24,22 @@ object NetworkUtil {
   val networkInfo = connectivityManager.activeNetworkInfo
   return networkInfo != null && networkInfo.isConnected
  }
+
+ fun isVpnActive(context: Context): Boolean {
+  val connectivityManager =
+   context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+//  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+  val network = connectivityManager.activeNetwork
+  val capabilities = connectivityManager.getNetworkCapabilities(network)
+  return capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_VPN) == true
+//  } else {
+  // For devices below API level 23
+//   return connectivityManager.allNetworks.any { network ->
+//    val networkInfo = connectivityManager.getNetworkInfo(network)
+//    networkInfo?.type == ConnectivityManager.TYPE_VPN && networkInfo.isConnected
+//   }
+//  }
+ }
+
 }
